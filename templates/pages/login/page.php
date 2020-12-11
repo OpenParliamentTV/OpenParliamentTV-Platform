ed9663835634a08066deefbbaa2a02634adca444
tammo0 <mrtammo@gmail.com>
2020-12-11 16:14:55 +0100

    bootstrap upgrade and htaccess fix
    bootstrap upgrade to 5
    .htaccess fix

--- templates/pages/login/page.php
+++ templates/pages/login/page.php
@@ -6,6 +6,28 @@
 			<h2><?php echo L::login; ?></h2>
 		</div>
 	</div>
+	<form class="needs-validation row g-3">
+
+			<div class="col-sm-6">
+				<label for="username" class="form-label">E-Mail</label>
+				<input type="text" class="form-control" id="username" placeholder="" value="" required="" name="user">
+				<div class="invalid-feedback">
+					E-Mail is required
+				</div>
+			</div>
+
+			<div class="col-sm-6">
+				<label for="password" class="form-label">Last name</label>
+				<input type="password" class="form-control" id="password" placeholder="" value="" required="" name="password">
+				<div class="invalid-feedback">
+					Valid last name is required.
+				</div>
+			</div>
+			<div class="col-12">
+				<button class="w-100 btn btn-primary" type="submit">Login</button>
+			</div>
+
+	</form>
 </main>
 <script type="text/javascript" src="client/js/jquery-3.4.1.min.js"></script>
 <script type="text/javascript" src="client/js/jquery-ui.min.js"></script>
