95d94301238259ca790b67bba7e708066e3a443e
tammo0 <mrtammo@gmail.com>
2021-01-25 12:50:41 +0100

    update ajaxServer.php

--- server/ajaxServer.php
+++ server/ajaxServer.php
@@ -86,12 +86,96 @@ switch ($_REQUEST["a"]) {
 		$return["text"] = "searchresults";
 
 		//TODO AUTH
+		//TODO Which Diff works for our case?
 		$media1 = getMedia($_REQUEST["v1"],$_REQUEST["p1"]);
 		$media2 = getMedia($_REQUEST["v2"],$_REQUEST["p2"]);
 		$return["return1diff2"] = arrayRecursiveDiff($media1, $media2);
 		$return["return2diff1"] = arrayRecursiveDiff($media2, $media1);
 		$return["returnV2_1diff2"] = array_diff_assoc_recursive($media1, $media2);
 		$return["returnV2_2diff1"] = array_diff_assoc_recursive($media2, $media1);
+
+	break;
+
+	case "mediaAdd":
+		require_once(__DIR__."/../modules/importtasks/functions.import.php");
+
+
+		$return["error"] = 0;
+
+		if (!array_key_exists($_REQUEST["parliament"],$config["parliament"])) {
+			$return["error"] = 1;
+			$tmpArray["parameter"] = "parliament";
+			$tmpArray["identifier"] = "#parliament";
+			$tmpArray["msg"] = "Parliament does not exist in config";
+			$return["reasons"][] = $tmpArray;
+		}
+
+		if (!$_REQUEST["electoralPeriod"]) {
+			$return["error"] = 1;
+			$tmpArray["parameter"] = "electoralPeriod";
+			$tmpArray["identifier"] = "#electoralPeriod";
+			$tmpArray["msg"] = "Field required";
+			$return["reasons"][] = $tmpArray;
+		}
+
+		if (!$_REQUEST["sessionNumber"]) {
+			$return["error"] = 1;
+			$tmpArray["parameter"] = "sessionNumber";
+			$tmpArray["identifier"] = "#sessionNumber";
+			$tmpArray["msg"] = "Field required";
+			$return["reasons"][] = $tmpArray;
+		}
+
+		if (!$_REQUEST["agendaItemTitle"]) {
+			$return["error"] = 1;
+			$tmpArray["parameter"] = "agendaItemTitle";
+			$tmpArray["identifier"] = "#agendaItemTitle";
+			$tmpArray["msg"] = "Field required";
+			$return["reasons"][] = $tmpArray;
+		}
+
+		if (!$_REQUEST["speakerFirstName"]) {
+			$return["error"] = 1;
+			$tmpArray["parameter"] = "speakerFirstName";
+			$tmpArray["identifier"] = "#speakerFirstName";
+			$tmpArray["msg"] = "Field required";
+			$return["reasons"][] = $tmpArray;
+		}
+
+		if (!$_REQUEST["speakerLastName"]) {
+			$return["error"] = 1;
+			$tmpArray["parameter"] = "speakerLastName";
+			$tmpArray["identifier"] = "#speakerLastName";
+			$tmpArray["msg"] = "Field required";
+			$return["reasons"][] = $tmpArray;
+		}
+
+		if (!$_REQUEST["mediaURL"]) {
+			$return["error"] = 1;
+			$tmpArray["parameter"] = "mediaURL";
+			$tmpArray["identifier"] = "#mediaURL";
+			$tmpArray["msg"] = "Field required";
+			$return["reasons"][] = $tmpArray;
+		}
+
+		if (!$_REQUEST["mediaOriginalURL"]) {
+			$return["error"] = 1;
+			$tmpArray["parameter"] = "mediaOriginalURL";
+			$tmpArray["identifier"] = "#mediaOriginalURL";
+			$tmpArray["msg"] = "Field required";
+			$return["reasons"][] = $tmpArray;
+		}
+
+		if ($return["error"] == 0) {
+			$return["success"] = "true";
+			$return["text"] = "mediaAdd";
+
+			//TODO AUTH for mediaAdd
+			$return["return"] = importParliamentMedia("forminput",$_REQUEST["parliament"],"",$_REQUEST);
+		}
+
+
+
 
 	break;
 
