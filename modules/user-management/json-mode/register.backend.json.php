b99860516013105de6acc208a12637631fdc780a
tammo0 <mrtammo@gmail.com>
2021-01-26 15:16:34 +0100

    move usermanagement json mode

--- modules/user-management/json-mode/register.backend.json.php
+++ modules/user-management/json-mode/register.backend.json.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(__DIR__."/../../config.php");
+require_once(__DIR__ . "/../../config.php");
 require_once(__DIR__."/functions.json.php");
 
 function registerUser($mail = "", $passwd = "", $name="") {
