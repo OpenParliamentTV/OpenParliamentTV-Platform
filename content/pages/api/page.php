3758fbc024808867bc973cd4d0be883025049876
Joscha Jaeger <joscha.jaeger@gmail.com>
2021-06-21 12:29:56 +0200

    remove api page

--- content/pages/api/page.php
+++
@@ -1,9 +0,0 @@
-<?php include_once(__DIR__ . '/../../header.php'); ?>
-<main class="container subpage">
-	<div class="row" style="position: relative; z-index: 1">
-		<div class="col-12">
-			<h2>API</h2>
-		</div>
-	</div>
-</main>
-<?php include_once(__DIR__ . '/../../footer.php'); ?>
