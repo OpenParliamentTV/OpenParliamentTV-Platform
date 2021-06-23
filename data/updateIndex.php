5ee7503330c15c8d483b764f2e53108beff57e4d
Joscha Jaeger <joscha.jaeger@gmail.com>
2021-06-23 14:44:58 +0200

    update index file

--- data/updateIndex.php
+++ data/updateIndex.php
@@ -11,7 +11,7 @@ require __DIR__.'/../vendor/autoload.php';
 $ESClient = Elasticsearch\ClientBuilder::create()->build();
 
 /*
-$response = $ESClient->indices()->delete(array("index"=>"bundestag_speeches"));
+$response = $ESClient->indices()->delete(array("index"=>"openparliamenttv_DE"));
 echo '<pre>';
 print_r($response);
 echo '</pre>';
@@ -74,7 +74,7 @@ function setOptions() {
 		)
 	);
 
-	$indexParams = array("index" => "bundestag_speeches", "body" => $data);
+	$indexParams = array("index" => "openparliamenttv_DE", "body" => $data);
 
 	try {
 		$result = $ESClient->indices()->create($indexParams);
@@ -103,7 +103,7 @@ function setMapping() {
 			"search_analyzer"=>"standard"
 	)));
 
-	$url = "http://localhost:9200/bundestag_speeches/_mapping?pretty";
+	$url = "http://localhost:9200/openparliamenttv_DE/_mapping?pretty";
 	$ch = curl_init( $url );
 
 	$payload = json_encode($data);
@@ -133,84 +133,19 @@ function updateIndex() {
 
 	global $ESClient;
 	
-	//$index_media = json_decode(file_get_contents("index_media.json"),true);
+	$data = '';
 
-	$index_people = json_decode(file_get_contents("index_people.json"),true);
-	$index_people_fallback = json_decode(file_get_contents("mdb_stammdaten.json"),true);
+	$docParams = array("index" => "openparliamenttv_DE", "id" => "DE-0190003001", "body" => $data);
 
-	$queueFiles = array_values(array_diff(scandir(__DIR__."/input"), array('.', '..', '.DS_Store', '_index.json', '.gitignore')));
-
-	foreach($queueFiles as $file) {
-
-		$index_media = json_decode(file_get_contents(__DIR__."/input/".$file),true);
-
-		foreach ($index_media as $k=>$m) {
-		
-			if (!isset($m["mediaID"]) || strlen( (string) $m["mediaID"] ) < 3 || !$m["aligned"]) {
-				continue;
-			}
-
-			$speakerInfo = $index_people[$m["speakerID"]];
-
-			if (!isset($speakerInfo)) {
-				
-				echo '<br><br>WARNING: Speaker info not found in AW data for speaker ID '.$m["speakerID"].'. Fallback to mdb_stammdaten.<br><br>';
-
-				$speakerInfo = $index_people_fallback[$m["speakerID"]];
-
-				if (!isset($speakerInfo)) {
-
-					echo 'ERROR: Speaker info not found in mdb_stammdaten for speaker ID '.$m["speakerID"].'.<br><br>';
-
-				}
-
-			}
-
-			$meta = array("id" => $m["id"],
-				"mediaID" => $m["mediaID"],
-				"duration" => $m["duration"],
-				"aligned" => (isset($m["aligned"])) ? $m["aligned"] : true,
-				"electoralPeriod" => $m["electoralPeriod"],
-				"sessionNumber" => $m["sessionNumber"],
-				"date" => $m["date"],
-				"timestamp" => $m["timestamp"],
-				"agendaItemTitle" => $m["agendaItemTitle"],
-				"agendaItemSecondTitle" => $m["agendaItemSecondTitle"],
-				"agendaItemThirdTitle" => $m["agendaItemThirdTitle"],
-				"documents" => $m["documents"],
-				"speakerID" => $m["speakerID"],
-				"speakerDegree" => (isset($speakerInfo["degree"])) ? $speakerInfo["degree"] : $m["speakerDegree"],
-				"speakerFirstName" => (isset($speakerInfo["first_name"])) ? $speakerInfo["first_name"] : $m["speakerFirstName"],
-				"speakerLastName" => (isset($speakerInfo["last_name"])) ? $speakerInfo["last_name"] : $m["speakerLastName"],
-				"speakerParty" => (isset($speakerInfo["party"])) ? $speakerInfo["party"] : $m["speakerParty"],
-				"speakerRole" => $m["speakerRole"],
-				"speakerGender" => $speakerInfo["gender"],
-				"speakerPicture" => $speakerInfo["picture"],
-				"aw_uuid" => $speakerInfo["aw_uuid"],
-				"aw_username" => $speakerInfo["aw_username"]);
-			
-			$content = htmlspecialchars_decode($m['content']);
-			
-			$data = array( "meta" => $meta,
-					"content" => $content);
-						
-			$docParams = array("index" => "bundestag_speeches", "id" => $m["id"], "body" => $data);
-
-			try {
-				$result = $ESClient->index($docParams);
-			} catch(Exception $e) {
-				$result = $e->getMessage();
-			}
-
-			echo '<pre>';
-			print_r($result);
-			echo '</pre>';
-
-		}
-
-		unlink(__DIR__."/input/".$file);
-
+	try {
+		$result = $ESClient->index($docParams);
+	} catch(Exception $e) {
+		$result = $e->getMessage();
 	}
+
+	echo '<pre>';
+	print_r($result);
+	echo '</pre>';
 
 }
 
