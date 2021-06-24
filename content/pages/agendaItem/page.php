2d5fb23a29942d7f8df8cb68938d8bfc4969a253
Joscha Jaeger <joscha.jaeger@gmail.com>
2021-06-24 15:40:45 +0200

    add electoralPeriod, session & agendaItem pages

---
+++ content/pages/agendaItem/page.php
@@ -0,0 +1,22 @@
+<?php include_once(__DIR__ . '/../../header.php'); ?>
+<main class="container subpage">
+	<div class="row" style="position: relative; z-index: 1">
+		<div class="col-12">
+			<h2>Detail View Agenda Item</h2>
+
+            <?php
+
+            include_once (__DIR__."/../../../api/v1/api.php");
+            $item = apiV1([
+            	"action"=>"getItem", 
+            	"itemType"=>"agendaItem", 
+            	"id"=>$_REQUEST["id"]]
+            );
+            print_r($item);
+
+            ?>
+
+		</div>
+	</div>
+</main>
+<?php include_once(__DIR__ . '/../../footer.php'); ?>
