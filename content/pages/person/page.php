e65dfe4db6756f3144eab25329bfdbd8016cf88a
Joscha Jaeger <joscha.jaeger@gmail.com>
2021-08-26 14:01:59 +0200

    add lang label for interventions

--- content/pages/person/page.php
+++ content/pages/person/page.php
@@ -41,7 +41,7 @@ $flatDataArray = flattenEntityJSON($apiResult["data"]);
 					<a class="nav-link active" id="media-tab" data-toggle="tab" href="#media" role="tab" aria-controls="media" aria-selected="true"><span class="icon-hypervideo"></span><span class="nav-item-label d-none d-sm-inline"><?php echo L::speeches ?></span></a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" id="interventions-tab" data-toggle="tab" href="#interventions" role="tab" aria-controls="interventions" aria-selected="true"><span class="icon-hypervideo"></span><span class="nav-item-label d-none d-sm-inline">Zwischenfragen</span></a>
+					<a class="nav-link" id="interventions-tab" data-toggle="tab" href="#interventions" role="tab" aria-controls="interventions" aria-selected="true"><span class="icon-hypervideo"></span><span class="nav-item-label d-none d-sm-inline"><?php echo L::interjections ?></span></a>
 				</li>
 				<li class="nav-item ml-auto">
 					<a class="nav-link" id="data-tab" data-toggle="tab" href="#data" role="tab" aria-controls="data" aria-selected="true"><span class="icon-download"></span><span class="nav-item-label d-none d-sm-inline"><?php echo L::data ?></span></a>
