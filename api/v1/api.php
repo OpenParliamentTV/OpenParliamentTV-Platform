1a58f2f08a67c5361305eec35f26943e7471c5a0
Joscha Jaeger <joscha.jaeger@gmail.com>
2021-08-27 13:13:08 +0200

    add license information to api

--- api/v1/api.php
+++ api/v1/api.php
@@ -13,6 +13,8 @@ function apiV1($request = false, $db = false, $dbp = false) { // TODO: action: g
 
     $return["meta"]["api"]["version"] = 1;
     $return["meta"]["api"]["documentation"] = $config["dir"]["root"]."/documentation/api";
+    $return["meta"]["api"]["license"]["label"] = "Open Database License (ODbL) v1.0";
+    $return["meta"]["api"]["license"]["link"] = "https://opendatacommons.org/licenses/odbl/1-0/";
     $return["meta"]["requestStatus"] = "error";
     $return["errors"] = array();
 
