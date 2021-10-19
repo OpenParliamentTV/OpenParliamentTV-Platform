1bfe3c6a9c4e99d8362eb32205ddc1a12804777b
Joscha Jaeger <joscha.jaeger@gmail.com>
2021-10-19 22:18:37 +0200

    update release notes

--- content/pages/version/page_de.php
+++ content/pages/version/page_de.php
@@ -3,15 +3,14 @@
 	<div class="row" style="position: relative; z-index: 1">
 		<div class="col-12">
 			<h2><?php echo L::releaseNotes; ?></h2>
-			<h3><?php echo L::versionCurrent; ?>: <br><b>Public Beta</b></h3>
-			<p class="mb-1">Die aktuelle Version ist öffentlich zugänglich, enthält aber z.T. fehlerhafte Daten, Beispieltexte und Bedienelemente die noch nicht funktionieren. Die meisten Module der Plattform wurden auf unterschiedlichen Geräten und in allen gängigen Browsern getestet. Die finalen Tests stehen jedoch noch aus. Zudem kann in dieser Version noch keine Barrierefreiheit gewährleistet werden. </p>
-			<div class="alert alert-warning py-1 px-2">Auch wenn alle Seiten bereits geteilt und eingebettet werden können ist es möglich, dass sich einzelne URLs und Parameter bis zum öffentlichen Launch noch ändern.</div>
+			<h3><?php echo L::versionPrevious; ?>: <br><b>Public Beta</b></h3>
+			<p class="mb-1">Die Public Beta Version war öffentlich zugänglich, enthielt aber z.T. noch fehlerhafte Daten, Beispieltexte und Bedienelemente die noch nicht funktionierten. </p>
 			<hr>
-			<h3><?php echo L::versionNext; ?>: <br><b>Öffentlicher Launch</b> <span class="icon-clock"></span> 20. Oktober 2021</h3>
-			<p>Ab 20. Oktober 2021 — kurz vor der konstituierenden Sitzung des 20. Deutschen Bundestages — wird die Open Parliament TV Plattform mit allen Funktionalitäten verfügbar und verwendbar sein. </p>
+			<h3><?php echo L::versionCurrent; ?>: <br><b>Öffentlicher Launch</b> <span class="icon-clock"></span> 20. Oktober 2021</h3>
+			<p>Seit 20. Oktober 2021 — kurz vor der konstituierenden Sitzung des 20. Deutschen Bundestages — ist die Open Parliament TV Plattform mit allen Funktionalitäten verfügbar und verwendbar. </p>
 			<hr>
 			<!--
-			<h3><?php echo L::versionPrevious; ?>:</h3>
+			<h3><?php echo L::versionNext; ?>:</h3>
 			-->
 		</div>
 	</div>
--- content/pages/version/page_en.php
+++ content/pages/version/page_en.php
@@ -3,15 +3,14 @@
 	<div class="row" style="position: relative; z-index: 1">
 		<div class="col-12">
 			<h2><?php echo L::releaseNotes; ?></h2>
-			<h3><?php echo L::versionCurrent; ?>: <br><b>Public Beta</b></h3>
-			<p class="mb-1">The current version is publicly accessible but might still contain faulty data, sample texts and buttons that don't work. Most modules have been tested on different devices and in recent browsers. The final tests are however still pending. Additionally, we can't guarantee accessibility in this version. </p>
-			<div class="alert alert-warning py-1 px-2">Although all pages can be shared and embedded, it is possible that specific URLs and parameters are subject to changes until the public launch. </div>
+			<h3><?php echo L::versionPrevious; ?>: <br><b>Public Beta</b></h3>
+			<p class="mb-1">The public beta version was publicly accessible but still contained faulty data, sample texts and buttons that didn't work. </p>
 			<hr>
-			<h3><?php echo L::versionNext; ?>: <br><b>Public Launch</b> <span class="icon-clock"></span> 20. October 2021</h3>
-			<p>On 20. October 2021 — a few days before the inaugural meeting of the 20. German Bundestag — the Open Parliament TV platform will be accessible and usable with all functionalities. </p>
+			<h3><?php echo L::versionCurrent; ?>: <br><b>Public Launch</b> <span class="icon-clock"></span> 20. October 2021</h3>
+			<p>Since 20. October 2021 — a few days before the inaugural meeting of the 20. German Bundestag — the Open Parliament TV platform is accessible and usable with all functionalities. </p>
 			<hr>
 			<!--
-			<h3><?php echo L::versionPrevious; ?>:</h3>
+			<h3><?php echo L::versionNext; ?>:</h3>
 			-->
 		</div>
 	</div>
