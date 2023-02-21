efdf12b31b09075dc26143080e5eef4055d68e15
tammo0 <dontsuckup@gmail.com>
2023-02-21 15:04:41 +0100

    Update function.entityDump.php

--- data/entity-dump/function.entityDump.php
+++ data/entity-dump/function.entityDump.php
@@ -34,7 +34,7 @@ function getEntityDump($parameter, $db = false) {
 
     if (($parameter["type"] == "person") || ($parameter["type"] == "all")) {
 
-        $people = $db->getAll("SELECT PersonID as id, PersonLabel as label, PersonLabelAlternative as labelAlternative, PersonFirstName as firstname, PersonLastName as lastname, 'person' as 'type' FROM ?n",$config["platform"]["sql"]["tbl"]["Person"]);
+        $people = $db->getAll("SELECT PersonID as id, PersonLabel as label, PersonLabelAlternative as labelAlternative, PersonFirstName as firstname, PersonLastName as lastname, 'person' as 'type', PersonType as subType FROM ?n",$config["platform"]["sql"]["tbl"]["Person"]);
         //echo $db->parse("SELECT PersonID as id, PersonLabel as label, PersonLabelAlternative as labelAlternative, PersonFirstName as firstname, PersonLastName as lastname, 'person' as 'type' FROM ?n",$config["platform"]["sql"]["tbl"]["Person"]);
         //print_r($people);
         foreach ($people as $k=>$person) {
@@ -47,7 +47,7 @@ function getEntityDump($parameter, $db = false) {
 
     if (($parameter["type"] == "organisation") || ($parameter["type"] == "all")) {
 
-        $organisations = $db->getAll("SELECT OrganisationID as id, OrganisationLabel as label, OrganisationLabelAlternative as labelAlternative, 'organisation' as 'type' FROM ?n",$config["platform"]["sql"]["tbl"]["Organisation"]);
+        $organisations = $db->getAll("SELECT OrganisationID as id, OrganisationLabel as label, OrganisationLabelAlternative as labelAlternative, 'organisation' as 'type', OrganisationType as subType FROM ?n",$config["platform"]["sql"]["tbl"]["Organisation"]);
         //TODO: When labelAlternative is already a json, dont do the following:
         foreach ($organisations as $k=>$organisation) {
             $organisations[$k]["labelAlternative"] = (($organisation["labelAlternative"]!=null) ? array($organisation["labelAlternative"]) : array());
@@ -59,7 +59,7 @@ function getEntityDump($parameter, $db = false) {
 
     if (($parameter["type"] == "term") || ($parameter["type"] == "all")) {
 
-        $terms = $db->getAll("SELECT TermWikidataID as id, TermLabel as label, TermLabelAlternative as labelAlternative, TermID as optvID, 'term' as 'type' FROM ?n",$config["platform"]["sql"]["tbl"]["Term"]);
+        $terms = $db->getAll("SELECT TermWikidataID as id, TermLabel as label, TermLabelAlternative as labelAlternative, TermID as optvID, 'term' as 'type', TermType as subType FROM ?n",$config["platform"]["sql"]["tbl"]["Term"]);
         //TODO: When labelAlternative is already a json, dont do the following:
         foreach ($terms as $k=>$term) {
             $terms[$k]["labelAlternative"] = array($term["labelAlternative"]);
@@ -71,7 +71,7 @@ function getEntityDump($parameter, $db = false) {
 
     if (($parameter["type"] == "document") || (($parameter["type"] == "all") && (!$parameter["wiki"]))) {
 
-        $documents = $db->getAll("SELECT DocumentWikidataID as id, DocumentLabel as label, DocumentLabelAlternative as labelAlternative, DocumentID as optvID, 'document' as 'type' FROM ?n",$config["platform"]["sql"]["tbl"]["Document"]);
+        $documents = $db->getAll("SELECT DocumentWikidataID as id, DocumentLabel as label, DocumentLabelAlternative as labelAlternative, DocumentID as optvID, 'document' as 'type', DocumentType as subType FROM ?n",$config["platform"]["sql"]["tbl"]["Document"]);
         //TODO: When labelAlternative is already a json, dont do the following:
         foreach ($documents as $k=>$document) {
             $documents[$k]["labelAlternative"] = array($document["labelAlternative"]);
