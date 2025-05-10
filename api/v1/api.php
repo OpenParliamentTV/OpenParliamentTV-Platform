150f5abab4b65afdb994a072aa105e32c8a653ee
Joscha Jaeger <joscha.jaeger@gmail.com>
2025-05-10 11:14:29 +0200

    show errors in log file

--- api/v1/api.php
+++ api/v1/api.php
@@ -1,8 +1,7 @@
 <?php
-error_reporting(0);
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
 
 require_once (__DIR__."/../../config.php");
 require_once ("config.php");
--- api/v1/index.php
+++ api/v1/index.php
@@ -2,8 +2,9 @@
 
 session_start();
 
-//error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
-error_reporting(0);
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
 
 header('Content-Type: application/json');
 
