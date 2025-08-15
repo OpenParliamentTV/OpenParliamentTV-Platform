0f1ac41f4a73c6ae5d4c35a4666aca270e09c12e
Joscha Jaeger <joscha.jaeger@gmail.com>
2025-08-15 16:26:51 +0200

    cleanup

--- modules/utilities/textArrayConverters.php
+++ modules/utilities/textArrayConverters.php
@@ -1,10 +1,5 @@
 <?php
 
-/*
-header('Content-Type: application/json');
-print_r(mergeAlignmentOutputWithTextObject($exampleAlignmentOutput, $exampleTextObject));
-*/
-
 function textObjectToHTMLString($inputTextObject, $mediaFileURI, $mediaID, $autoAddIDs = false) {
 	
 	$sentenceID = 0;
@@ -91,69 +86,6 @@ function simpleTextBodyArrayToHTMLString($textBody) {
 	return $outputHTML;
 }
 
-function textObjectToAlignmentInput($inputTextObject, $mediaFileURI, $mediaID) {
-	
-	$outputXML = '<?xml version="1.0" encoding="UTF-8"?><html xmlns="http://www.w3.org/1999/xhtml"><head><meta charset="utf-8"/></head><body>';
-
-	$outputXML .= textObjectToHTMLString($inputTextObject, $mediaFileURI, $mediaID, true);
-
-	$outputXML .= '</body></html>';
-
-	return $outputXML;
-}
-
-function mergeAlignmentOutputWithTextObject($alignmentOutput, $inputTextObject) {
-	
-	$fragmentCnt = 0;
-
-	if (is_string($inputTextObject)) {
-		$inputTextObject = json_decode($inputTextObject, 1);
-		if (!$inputTextObject) {
-			echo 'Input text could not be parsed as JSON.';
-		}
-	} else {
-		echo 'Input text needs to be a String';
-	}
-
-	if (is_string($alignmentOutput)) {
-		$alignmentOutput = json_decode($alignmentOutput, 1);
-		if (!$alignmentOutput) {
-			echo 'Alignment Output could not be parsed as JSON.';
-		}
-	} else {
-		echo 'Alignment Output needs to be a String';
-	}
-
-	foreach ($inputTextObject['textBody'] as $paragraphIndex => $paragraph) {
-		
-		if ($paragraph['type'] == 'speech') {
-			foreach ($paragraph['sentences'] as $sentenceIndex => $sentence) {
-				$fragmentID = 's'.sprintf('%06d', ++$fragmentCnt);
-				foreach ($alignmentOutput['fragments'] as $fragment) {
-					if ($fragment['id'] == $fragmentID) {
-						
-						$inputTextObject['textBody'][$paragraphIndex]['sentences'][$sentenceIndex]['timeStart'] = $fragment['begin'];
-						$inputTextObject['textBody'][$paragraphIndex]['sentences'][$sentenceIndex]['timeEnd'] = $fragment['end'];
-
-					}
-				}
-			}
-		}
-
-		$inputTextObject['textBody'][$paragraphIndex]['text'] = simpleTextBodyArrayToHTMLString($inputTextObject['textBody'][$paragraphIndex]);
-	}
-
-	$newTextHTML = '';
-	foreach ($inputTextObject['textBody'] as $paragraphIndex => $paragraph) {
-		foreach ($paragraph['sentences'] as $sentenceIndex => $sentence) {
-			$newTextHTML .= $inputTextObject['textBody'][$paragraphIndex]['text'];
-		}
-	}
-	$inputTextObject['textHTML'] = $newTextHTML;
-
-	return json_encode($inputTextObject, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-
-}
 
 
 ?>
