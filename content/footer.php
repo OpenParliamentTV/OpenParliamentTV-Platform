f039ac2ecfa07896efc11a7c47660dfd25f652f2
tammo0 <dontsuckup@gmail.com>
2025-12-22 14:52:40 +0100

    dynamic faction colors css & json fix

--- content/footer.php
+++ content/footer.php
@@ -14,14 +14,34 @@
 
     $factionColors = [];
     $factionIDColors = [];
+    $factionCSS = [];
     foreach ($factions["data"] as $faction) {
         $factionColors[$faction["attributes"]["label"]] = $faction["attributes"]["color"];
         $factionIDColors[$faction["id"]] = $faction["attributes"]["color"];
+
+        $tmpFactionCSS = "
+        .partyIndicator[data-party='".$faction["attributes"]["label"]."'],
+        .partyIndicator[data-faction='".$faction["attributes"]["label"]."'],
+        .partyIndicator[data-party='".$faction["id"]."'],
+        .partyIndicator[data-faction='".$faction["id"]."']";
+        foreach ($faction["attributes"]["labelAlternative"] as $labelAlternative) {
+            $tmpFactionCSS .= ", .partyIndicator[data-faction='".$labelAlternative."'], .partyIndicator[data-faction='".$labelAlternative."']";
+        }
+        $tmpFactionCSS .= " { background-color:".$faction["attributes"]["color"]."; border-color:".$faction["attributes"]["color"]."; background: var(--primary-bg-color); }";
+        $factionsCSS[] = $tmpFactionCSS;
     }
-    echo "var factionColors=".json_decode($factionColors);
-    echo "var factionIDColors=".json_decode($factionIDColors);
+
+    echo "var factionColors=".json_encode($factionColors, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_HEX_QUOT | JSON_HEX_APOS | JSON_FORCE_OBJECT).";\n";
+    echo "var factionIDColors=".json_encode($factionIDColors, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_HEX_QUOT | JSON_HEX_APOS | JSON_FORCE_OBJECT);
 
     ?>
 
 </script>
+<style type="text/css">
+    <?php
+    foreach ($factionsCSS as $factionCSS) {
+        echo $factionCSS;
+    }
+    ?>
+</style>
 <script type="text/javascript" src="<?= $config["dir"]["root"] ?>/content/client/js/generic.js?v=<?= $config["version"] ?>"></script>
